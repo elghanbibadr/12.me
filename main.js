1bc440d11e3aace57fb4fade562011e1b63eed20
badr Ghanbi <bghanbi50@gmail.com>
2022-01-25 22:16:54 +0100

    link

---
+++ main.js
@@ -0,0 +1,8 @@
+const numbers=[1,2,3];
+const employee=[
+    {name:'a',id:'b'}
+];
+const companyIsBreaking=false;
+if (companyIsBreaking)employee.push(  {name:'a',id:'x'},  {name:'a',id:'v'})
+console.log(employee);
+
