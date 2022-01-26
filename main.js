56816ab7be76e9126cd8441445a8d6c64df6e3f7
badr Ghanbi <bghanbi50@gmail.com>
2022-01-26 18:08:41 +0100

    lists

--- main.js
+++
@@ -1,8 +0,0 @@
-const numbers=[1,2,3];
-const employee=[
-    {name:'a',id:'b'}
-];
-const companyIsBreaking=false;
-if (companyIsBreaking)employee.push(  {name:'a',id:'x'},  {name:'a',id:'v'})
-console.log(employee);
-
